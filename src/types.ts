5ea5d628061709e375ce57fb2a5d00ee5b6766f2
Igor Michael <igormichael120@gmail.com>
2021-12-22 20:39:26 -0300

    Challenge complete

---
+++ src/types.ts
@@ -0,0 +1,15 @@
+export interface food {
+  id: number
+  name: string
+  description: string
+  price: number
+  available: boolean
+  image: string
+}
+
+export interface dataInput {
+  image: string
+  name: string
+  price: string
+  description: string
+}
